916e77978bf5adb712d270e50c26afc92a605aee
Yoinel Martinez <yoinelmartinez04@gmail.com>
2026-03-12 17:15:53 -0500

    realizo el ejercicio 2 de el js del punto c

--- punto1.js
+++ punto1.js
@@ -3,3 +3,12 @@ console.log("8 <= 8 =", 8 <= 8);
 console.log("7 != '7' =", 7 != "7");
 console.log("4 === 4 =", 4 === 4);
 console.log("6 == '6' =", 6 == "6");
+
+//punto 2 
+
+// OPERADORES LOGICOS
+
+console.log((true && true) || false);
+console.log((5 > 2) && (10 < 8));
+console.log(!(4 === "4"));
+console.log((3 <= 3) || (7 > 10));
